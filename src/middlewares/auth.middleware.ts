cb0b4d24e379beaa4986b253f38fdbfe5a1dfa91
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-06-28 16:21:56 +0300

    style: use AuthRequest interface

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,18 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { jwtConfig } from '../config/jwt';
+import { AuthRequest } from '@/types/auth-request.type';
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    next({ message: 'Authentication failed' });
+    return;
+  }
+
   try {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    }
-
     const decoded = jwt.verify(token, jwtConfig.secret) as { userId: string };
     req.user = { id: decoded.userId };
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Authentication failed' });
+    next({ message: 'Authentication failed' });
   }
 };
