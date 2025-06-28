91d23896164df11328e38730c21610a6c819c730
uzdenovabdullakh <uzdenovabdullah@yandex.ru>
2025-06-28 10:29:51 +0300

    feat: create index routes, add to app

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import sequelize from './config/sequelize';
 import config from './config/env'
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
 
 const PORT = config.APP_PORT;
 
